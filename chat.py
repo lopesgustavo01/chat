89f27b0374527e1a21e1f8d82a3fe2917491d4f3
Luiz Gustavo S Lopes <lglopes1789@gmail.com>
2023-05-18 20:03:41 -0300

    v5

--- chat.py
+++ chat.py
@@ -2,5 +2,5 @@ from app import app, io
 import os
 
 if __name__ == '__main__':
-    port = int(os.getenv('PORT', '5000'))
-    io.run(app, host='0.0.0.0', port=port)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)

5cff511e2d419002460aeeffc82e2f152ae7ba97
Luiz Gustavo S Lopes <lglopes1789@gmail.com>
2023-05-18 18:01:32 -0300

    v1

--- app.py
+++ app.py
@@ -1,22 +1,33 @@
-from flask import Flask, render_template
-from flask_socketio import SocketIO, send
+from flask.app import Flask
+from flask.templating import render_template
+from flask_socketio import SocketIO, emit, send
 
 app = Flask(__name__)
-app.config['SECRET'] = "secret!123"
-socketio = SocketIO(app, cors_allowed_origins="*")
+io = SocketIO(app)
 
-
-@socketio.on('message')
-def handle_message(message):
-    print("Received message: " + message)
-    if message != "User connected!":
-        send(message, broadcast=True)
+messages = []
 
 
 @app.route('/')
-def index():
+def home():
     return render_template("index.html")
 
 
+@io.on('sendMessage')
+def send_message_handler(msg):
+    global messages
+    if len(messages) > 10:
+        messages = messages[1:]
+        messages.append(msg)
+    else:
+        messages.append(msg)
+    emit('getMessage', msg, broadcast=True)
+
+
+@io.on('message')
+def message_handler(msg):
+    send(messages)
+
+
 if __name__ == "__main__":
-    socketio.run(app, host="localhost")
+    io.run(app, debug=True)
